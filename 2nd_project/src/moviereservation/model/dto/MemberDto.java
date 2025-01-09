ca95d96381a3dffd224ecf1611cc3a691da60e14
gitminwoo817 <gitcapzhang123@naver.com>
2025-01-09 17:06:27 +0900

    장민우 브랜치 생성, Dto 코드 작성

--- 2nd_project/src/moviereservation/model/dto/MemberDto.java
+++ 2nd_project/src/moviereservation/model/dto/MemberDto.java
@@ -1,5 +1,54 @@
 package moviereservation.model.dto;
 
 public class MemberDto {
-
+	int memberId;
+	String mid;
+	String mpw;
+	String mname;
+	String mbirth;
+	public MemberDto() {}
+	public MemberDto(int memberId, String mid, String mpw, String mname, String mbirth) {
+		super();
+		this.memberId = memberId;
+		this.mid = mid;
+		this.mpw = mpw;
+		this.mname = mname;
+		this.mbirth = mbirth;
+	}
+	@Override
+	public String toString() {
+		return "MemberDto [memberId=" + memberId + ", mid=" + mid + ", mpw=" + mpw + ", mname=" + mname + ", mbirth="
+				+ mbirth + "]";
+	}
+	public int getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
+	public String getMid() {
+		return mid;
+	}
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+	public String getMpw() {
+		return mpw;
+	}
+	public void setMpw(String mpw) {
+		this.mpw = mpw;
+	}
+	public String getMname() {
+		return mname;
+	}
+	public void setMname(String mname) {
+		this.mname = mname;
+	}
+	public String getMbirth() {
+		return mbirth;
+	}
+	public void setMbirth(String mbirth) {
+		this.mbirth = mbirth;
+	}
+	
 }
