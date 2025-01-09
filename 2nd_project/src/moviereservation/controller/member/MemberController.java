3da239897af2d827d6ca4c84ba2abcd888150463
gitminwoo817 <gitcapzhang123@naver.com>
2025-01-09 18:10:48 +0900

    view, controller, dao 회원가입 계속 실패, 수정 필요

--- 2nd_project/src/moviereservation/controller/member/MemberController.java
+++ 2nd_project/src/moviereservation/controller/member/MemberController.java
@@ -1,5 +1,15 @@
 package moviereservation.controller.member;
 
-public class MemberController {
+import moviereservation.model.dao.member.MemberDao;
+import moviereservation.model.dto.member.MemberDto;
 
+public class MemberController {
+	private static MemberController instance = new MemberController();
+	private MemberController() {}
+	public static MemberController getInstance() {return instance;}
+	
+	public boolean signup(MemberDto memberDto) {
+		boolean result = MemberDao.getInstance().signup(memberDto);
+		return result;
+	}
 }
--- 2nd_project/src/moviereservation/view/member/MemberView.java
+++ 2nd_project/src/moviereservation/view/member/MemberView.java
@@ -2,6 +2,9 @@ package moviereservation.view.member;
 
 import java.util.Scanner;
 
+import moviereservation.controller.member.MemberController;
+import moviereservation.model.dto.member.MemberDto;
+
 public class MemberView {
 	private static MemberView instance = new MemberView();
 	private MemberView() {}
@@ -12,12 +15,20 @@ public class MemberView {
 		while(true) {
 			System.out.println("1.회원가입 2.로그인");
 			int choose = scan.nextInt();
-			if(choose == 1) {}
+			if(choose == 1) {signup();}
 			else if(choose ==2) {}
 		}
 	}
 	
 	public void signup() {
-		System.out.println("아이디"); 
+		System.out.println("아이디"); String mid = scan.next();
+		System.out.println("비밀번호"); String mpw = scan.next();
+		System.out.println("이름"); String mname = scan.next();
+		System.out.println("생년월일"); String mbirth = scan.next();
+		MemberDto memberDto = new MemberDto(mid, mpw, mname, mbirth);
+		boolean result = MemberController.getInstance().signup(memberDto);
+		if(result) {System.out.println("회원가입성공");}
+		else {System.out.println("회원가입실패");}
+		
 	}
 }
