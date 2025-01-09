1708e8e0cd5e9112304adc7492fe1425f2d6763a
Ryuyena0305 <Ryuyena0305@gmail.com>
2025-01-09 16:44:53 +0900

    day01 : 공통 사용코드

--- 2nd_project/src/moviereservation/AppStart.java
+++ 2nd_project/src/moviereservation/AppStart.java
@@ -1,10 +1,22 @@
 package moviereservation;
 
+import java.util.Scanner;
+
+import moviereservation.view.AdminView;
+import moviereservation.view.MemberView;
+
 public class AppStart {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);	
+		System.out.println("1.관리자 2.사용자");
+		int choose = scan.nextInt();
+		if(choose ==1 ) {
+			AdminView.getInstance().run();
+		}
+		else if(choose == 2) {
+			MemberView.getInstance().run();
+		}
 	}
 
 }
--- 2nd_project/src/moviereservation/view/MemberView.java
+++ 2nd_project/src/moviereservation/view/MemberView.java
@@ -1,5 +1,11 @@
 package moviereservation.view;
 
 public class MemberView {
-
+	private static MemberView instance = new MemberView();
+	private MemberView() {}
+	public static MemberView getInstance() {return instance;}
+	
+	public void run() {
+		
+	}
 }
